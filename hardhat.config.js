36959e0a37e5db090909d7f0be90045e8f6d343a
Thanh Tâm <32576507+thanhtamv2t@users.noreply.github.com>
2025-03-28 03:07:59 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,13 +19,10 @@ module.exports = {
   },
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/703c33e04a1649088ed0537ef0965779",
-      // url: "https://blockchain.googleapis.com/v1/projects/map-blog-2e29d/locations/asia-east1/endpoints/ethereum-sepolia/rpc?key=AIzaSyCWrwgD9Agj-KrOM1tQ6A2hUV8lU4QzuKg",
-      accounts: ["046b0f39761b1667b4e29940c00eeb776358042a10354b5fe7a837a38083f2dd","40c30a12dcd4c3922f4817a8b03c431851182a62d1f0ebb48ce2d3768ddbda6f"]
+
     },
     binance: {
       url: 'https://bsc-testnet-rpc.publicnode.com',
-      accounts: ["046b0f39761b1667b4e29940c00eeb776358042a10354b5fe7a837a38083f2dd","40c30a12dcd4c3922f4817a8b03c431851182a62d1f0ebb48ce2d3768ddbda6f"]
     }
   },
   // etherscan: {
